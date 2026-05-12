9be08e29061e91b9bccf457265b28fcfdde6ef2d
vale <valeryperalta2712@gmail.com>
2026-05-12 09:07:55 -0400

    commit raulito

---
+++ src/main/java/model/rol.java
@@ -0,0 +1,23 @@
+package model;
+
+import jakarta .persistence.*;
+
+@Entity
+@table (name = "roles")
+public class Rol {
+
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column (nullable = false, unique = true, length = 30)
+    private String nombre;
+
+    @Column(nullable = false, length = 100)
+    private String descripcion;
+
+    public Integer getId()          { return id; }
+    public String getNombre()       { return nombre; }
+    public void setNombre(String n) { this.nombre = n; }
+    public String getDescripcion()  { return descripcion; }
+}

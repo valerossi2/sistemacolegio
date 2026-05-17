085f9aacee16de93dab927b1d6f1bf02ff4a78f6
vale <valeryperalta2712@gmail.com>
2026-05-17 15:46:46 -0400

    commiteddd

---
+++ src/main/java/model/Horario.java
@@ -0,0 +1,13 @@
+package model;
+import jakarta.persistence.*;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "horarios")
+public class Horario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+}

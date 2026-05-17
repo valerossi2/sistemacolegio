99cce0c0041d2c97bede9a5cc5fafebaaadd26fc
vale <valeryperalta2712@gmail.com>
2026-05-17 15:30:53 -0400

    commiteddd

--- src/main/java/model/SesionLog.java
+++ src/main/java/model/SesionLog.java
@@ -13,7 +13,35 @@ public class SesionLog {
     @JoinColumn(name = "usuario_id", nullable = false)
     private Usuario usuario;
 
+    @Column(name = "ip_adress", length = 45)
+    private String ipAddress;
 
+    @Column(name = "user_agent", length = 512)
+    private String userAgent;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "metodo_auth", nullable = false)
+    private MetodoAuth metodoAuth;
+
+    @Column(name = "creado_en", updatable = false)
+    private LocalDate CreadoEn;
+
+
+    public enum MetodoAuth { GOOGLE, LOCAL }
+
+    @PrePersist
+    protected void onCreate() { CreadoEn = LocalDate.now(); }
+
+    public Long getId()                    { return id; }
+    public Usuario getUsuario()            { return usuario; }
+    public void setUsuario(Usuario u)      { this.usuario = u; }
+    public String getIpAddress()           { return ipAddress; }
+    public void setIpAddress(String ip)    { this.ipAddress = ip; }
+    public String getUserAgent()           { return userAgent; }
+    public void setUserAgent(String ua)    { this.userAgent = ua; }
+    public MetodoAuth getMetodoAuth()      { return metodoAuth; }
+    public void setMetodoAuth(MetodoAuth m){ this.metodoAuth = m; }
+    public LocalDate getCreadoEn()     { return CreadoEn; }
 
 
 
